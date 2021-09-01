f1007dbfdf942da4c050fec6e16ef2403219a678
Astatham98 <alex.statham1998@gmail.com>
2021-09-01 22:13:41 +0100

    New colours and ability to update
    Setup when ran again now changes all of the colours to the ones specified in the div_colours dictionary

--- commands/setup.py
+++ commands/setup.py
@@ -1,6 +1,6 @@
 from commands.base_command  import BaseCommand
-import div_colors as col
-
+from div_colors import div_colours
+import discord
 
 class setup(BaseCommand):
 
@@ -15,39 +15,32 @@ class setup(BaseCommand):
         role_names = [x.name for x in roles]
         admin = message.author.guild_permissions.administrator
         if 'ETF2L - Premiership' not in role_names and admin:
+            for name, col in div_colours.items():
+                print(name, col)
+                await self.create_role(message, name, col)
             
-            #AsiaFortress
-            await self.create_role(message, 'AsiaFortress - Division 1', col.PREM)
-            await self.create_role(message, 'AsiaFortress - Division 2', col.DIV2)
-            await self.create_role(message, 'AsiaFortress - Division 3', col.MID)
-            await self.create_role(message, 'AsiaFortress - Division 4', col.OPEN)
-
-            #ETF2L
-            await self.create_role(message, 'ETF2L - Premiership', col.PREM)
-            await self.create_role(message, 'ETF2L - Division 1', col.DIV1)
-            await self.create_role(message, 'ETF2L - Division 2', col.DIV2)
-            await self.create_role(message, 'ETF2L - Mid', col.MID)
-            await self.create_role(message, 'ETF2L - Low', col.LOW)
-            await self.create_role(message, 'ETF2L - Open', col.OPEN)
-
-            #Ozfortress
-            await self.create_role(message, 'OzFortress - Premier', col.PREM)
-            await self.create_role(message, 'OzFortress - Intermediate', col.DIV2)
-            await self.create_role(message, 'OzFortress - Main', col.MID)
-            await self.create_role(message, 'OzFortress - Open', col.OPEN)
-
-            #RGL
-            await self.create_role(message, 'RGL', col.RGL)
-
             await message.channel.send('Roles have been added')
             print('Roles added')
         elif not admin:
             print('Not admin')
             await message.channel.send('You do not have permission to run this command')
         else:
+            await self.edit_roles(message)
             print('Has roles')
             await message.channel.send('This server already has the required roles')
 
+    
     async def create_role(self, message, dregion, color):
         guild = message.guild
         await guild.create_role(name=dregion, color=color)
+    
+    
+    async def edit_roles(self, message):
+        roles = await message.guild.fetch_roles()
+        for role in roles:
+            try:
+                color = div_colours.get(role.name, role.colour)
+                await role.edit(colour=color)
+            except discord.errors.Forbidden:
+                pass
+            
--- div_colors.py
+++ div_colors.py
@@ -1,7 +1,17 @@
-PREM = 0x9000FF
-DIV1 = 0x4F00FF
-DIV2 = 0x00C9FF
-MID = 0x00FF88
-LOW = 0x00FF00
-OPEN = 0xD0FF00
-RGL = 0xFF9E00
+div_colours = {
+    'AsiaFortress - Division 1': 0x4A235A,
+    'AsiaFortress - Division 2': 0x6C3483,
+    'AsiaFortress - Division 3': 0x8E44AD,
+    'AsiaFortress - Division 4': 0xBB8FCE,
+    'ETF2L - Premiership': 0x1B4F72,
+    'ETF2L - Division 1': 0x21618C,
+    'ETF2L - Division 2': 0x2874A6,
+    'ETF2L - Mid': 0x3498DB,
+    'ETF2L - Low': 0x5DADE2,
+    'ETF2L - Open': 0x85C1E9,
+    'OzFortress - Premier': 0x186A3B,
+    'OzFortress - Intermediate': 0x239B56,
+    'OzFortress - Main': 0x2ECC71,
+    'OzFortress - Open': 0x82E0AA,
+    'RGL':  0xFF9E00
+}
