8bcf96d5abbba6f9b572ec1736e572858b1cf0a1
Astatham98 <alex.statham1998@gmail.com>
2021-09-02 12:13:24 +0100

    Fixed Ozfortress logic
    If a player has past matches it uses that, if a user has no matches it uses the current team they're in

--- divgetters/ozf_player_id_get.py
+++ divgetters/ozf_player_id_get.py
@@ -8,11 +8,13 @@ class Ozfortress():
         doc = requests.get(league).text
         soup = BeautifulSoup(doc, 'lxml')
         
-        teams = soup.find_all(class_='row ml-0')
+        teams = soup.find_all(class_='list-group list-group-flush mb-4')
+        teams = soup.find_all(class_='row ml-0') if not teams else teams
         for team in teams:
             if 'ultiduo' not in team.text.strip().lower():
                 newest_team = team.text.strip()
                 break
+            
         try:
             league = re.findall(r' in (.*?) on ',newest_team)[0]
         except IndexError:
