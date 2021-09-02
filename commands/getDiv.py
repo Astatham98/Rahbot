91bb171e88e2234f763b21712128de59d3c5b007
Astatham98 <alex.statham1998@gmail.com>
2021-09-02 14:16:32 +0100

    New remove and embed messages
    Removes all messages from a channel after a user has gotten a role and then posts an embed for new users

--- commands/getDiv.py
+++ commands/getDiv.py
@@ -4,6 +4,8 @@ from random                 import randint
 from divgetters.etf2l_player_id_get import Etf2l
 from divgetters.ozf_player_id_get import Ozfortress
 from divgetters.asia_player_id_get import AsiaFortress
+from time import sleep
+import discord
 
 class Div(BaseCommand):
 
@@ -17,19 +19,21 @@ class Div(BaseCommand):
         params = ["Player page"]
         super().__init__(description, params)
 
-    async def handle(self, params, message, client):
+    async def handle(self, params, msg, client):
         div = self.get_region(params[0])
 
-        roles = message.guild.roles
+        roles = msg.guild.roles
         for role in roles:
             if role.name.lower() in div.lower():
                 if div.lower() == 'rgl':
-                    await message.author.add_roles(role)
-                    await message.channel.send('{} has been given the {} role. Tell Sigafoo to make a public API if you want divs.'.format(message.author.mention, div))
+                    await msg.author.add_roles(role)
+                    await msg.channel.send('{} has been given the {} role. Tell Sigafoo to make a public API if you want divs.'.format(message.author.mention, div))
+                    await self.purge_and_post(msg)
                     break
                 else:
-                    await message.author.add_roles(role)
-                    await message.channel.send('{} has been given the {} role.'.format(message.author.mention, div))
+                    await msg.author.add_roles(role)
+                    await msg.channel.send('{} has been given the {} role.'.format(msg.author.mention, div))
+                    await self.purge_and_post(msg)
                     break
 
 
@@ -42,3 +46,14 @@ class Div(BaseCommand):
             return AsiaFortress().get_div(link)
         elif 'ozfortress' in link:
             return Ozfortress().get_div(link)
+        
+    async def welcome_message_embed(self, message):
+        embed=discord.Embed(title="Step up and get your role!", description="Get your own role based on your division in any region!", color=0xe69100)
+        embed.add_field(name="Simply type ;div {region link here}", value="Use; etf2l for EU, RGL for NA, match.tf for Asia Pacific, and OzFortress for AU.", inline=True)
+        embed.set_footer(text="Enjoy your say :).")
+        await message.channel.send(embed=embed)
+        
+    async def purge_and_post(self, msg):
+        sleep(1)
+        await msg.channel.purge()
+        await self.welcome_message_embed(msg)
