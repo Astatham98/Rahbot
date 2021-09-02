079b4915663d37e55d0388537b39b975b362dd5a
Astatham98 <alex.statham1998@gmail.com>
2021-09-02 09:21:45 +0100

    Updated etf2l logic
    Will now get your newest game played where you didn't merc

--- divgetters/etf2l_player_id_get.py
+++ divgetters/etf2l_player_id_get.py
@@ -13,17 +13,20 @@ class Etf2l():
         if response.status_code == 200:
             json_format = response.json()
             competitions = json_format['results']
-            divs = []
+            divs = ""
             try:
                 for c in competitions:
                     comp = c['competition']
-                    if comp['type'] == '6on6':
+                    if comp['type'] == '6on6' and c['merced'] == 0:
                         if c['division']['name'] is not None:
-                            divs.append(c['division']['name'])
+                            divs = c['division']['name']
+                            break
             except TypeError:
-                divs.append('Open')
+                divs = 'Open'
             
-            count = Counter(divs)
-            return 'Etf2l - ' + count.most_common()[0][0]
+            print(divs)
+            #count = Counter(divs)
+            #return 'Etf2l - ' + count.most_common()[0][0]
+            return 'Etf2l - ' + divs
 
 
