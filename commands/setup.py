f25eb66928f8fa28f208111d762d402bd8ab4fe0
Astatham98 <alex.statham1998@gmail.com>
2021-09-03 12:19:01 +0100

    Setup now adds highlander roles to the server

--- commands/setup.py
+++ commands/setup.py
@@ -14,20 +14,27 @@ class setup(BaseCommand):
         roles = await message.guild.fetch_roles()
         role_names = [x.name for x in roles]
         admin = message.author.guild_permissions.administrator
-        if 'ETF2L - Premiership' not in role_names and admin:
-            for name, col in div_colours.items():
-                print(name, col)
-                await self.create_role(message, name, col)
-            
-            await message.channel.send('Roles have been added')
-            print('Roles added')
-        elif not admin:
-            print('Not admin')
-            await message.channel.send('You do not have permission to run this command')
-        else:
-            await self.edit_roles_color(message)
-            print('Has roles')
-            await message.channel.send('This server already has the required roles')
+        
+        for gamemode in ["6's", "highlander"]:
+            if 'ETF2L - Premiership' + ' ' + gamemode not in role_names and admin:
+                #Excludes rgl and newb role from getting a gamemode tag
+                for name, col in list(div_colours.items())[:-2]:
+                    print(name, col)
+                    await self.create_role(message, name + ' ' + gamemode, col)
+                
+                #If RGL or newb role don't exist, add them 
+                for name, col in list(div_colours.items())[:-2]:
+                    if name not in role_names:
+                        await self.create_role(message, name, col)
+                await message.channel.send(gamemode + ' Roles have been added')
+                print('Roles added')
+            elif not admin:
+                print('Not admin')
+                await message.channel.send('You do not have permission to run this command')
+            else:
+                #await self.edit_roles_color(message)
+                print('Has roles')
+                await message.channel.send('This server already has the required roles for ' + gamemode)
 
     
     async def create_role(self, message, dregion, color):
