97e44c36cb9b131905fed7f6e50bf1744121b246
Astatham98 <alex.statham1998@gmail.com>
2021-09-10 16:05:51 +0100

    start of autobalance

--- autobalance/autobalance.py
+++ autobalance/autobalance.py
@@ -2,4 +2,6 @@ from autobalance import getRanks
 from autobalance import sumsplit
 
 def autobalance(members, message):
-    pass
+    rank_nums = getRanks.get_ranks(members, message)
+    split = sumsplit.sumSplit(list(rank_nums.values()))
+    print(split)
