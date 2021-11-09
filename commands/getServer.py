729cddb1a4b512314a10f1e924ced60521adfca1
Astatham98 <alex.statham1998@gmail.com>
2021-11-09 18:30:18 +0000

    Spelling

--- commands/getServer.py
+++ commands/getServer.py
@@ -20,4 +20,4 @@ class Getserver(BaseCommand):
             await message.channel.send('Sever IP is: \n {}'.format(server))
         
             dm_channel = await message.author.create_dm()
-            await dm_channel.send("The rcon password for this servr is: {}".format(rcon))
+            await dm_channel.send("The rcon password for this server is: {}".format(rcon))
