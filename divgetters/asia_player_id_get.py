387aceb1bd4662ea344992e816b993e24a678f27
Astatham98 <alex.statham1998@gmail.com>
2022-01-03 15:44:41 +0000

    Asiafortress fix

--- divgetters/asia_player_id_get.py
+++ divgetters/asia_player_id_get.py
@@ -13,7 +13,7 @@ class AsiaFortress():
         i=0
         found = False
         while i <= len(lists)-1 and not found:
-            if 'asiafortress cup' in lists[i].text.lower():
+            if 'played in asiafortress cup' in lists[i].text.lower():
                 for x in lists[i].find_all('a', href=True):
                     if 'team' in x['href']:
                         team = x['href']
@@ -21,11 +21,13 @@ class AsiaFortress():
             i+=1
         
         team_link = 'https://match.tf' + team if team != None else None
+        print(team_link)
         if team_link != None:
             doc = requests.get(team_link).text
             soup = BeautifulSoup(doc, 'lxml')
             lists = soup.find_all('div', class_='panel-heading')
-            posdiv = [x.text.strip() for x in lists if 'AsiaFortress' in x.text.strip() and 'placed' in x.text.strip()][0]
+            posdiv = [x.text.strip() for x in lists if 'AsiaFortress Cup' in x.text][0]
             return ['AsiaFortress - ' + re.findall(r' in (.*?) for ',posdiv)[0] + ' ' + "6's"]
         else:
             return ['AsiaFortress - Division 4' + ' ' + "6's"]
+        
