f3de53f553a55c2f73dd9fa5f69bcfbf2794e3a2
Astatham98 <alex.statham1998@gmail.com>
2022-01-06 17:42:19 +0000

    Error handling and user pick list

--- commands/medicpicker.py
+++ commands/medicpicker.py
@@ -2,6 +2,7 @@ from commands.base_command import BaseCommand
 import settings
 from database import Database
 import random
+import time
 
 
 class MedicPicker(BaseCommand):
@@ -26,9 +27,11 @@ class MedicPicker(BaseCommand):
                     if self.db.immune(player) is False:
                         eligible.append(player)
                 
+                await message.channel.send(f'Picking from {", ".join([client.fetch_user(int(x)) for x in eligible])}')
                 chosen_player_id = random.choice(eligible)
                 chosen_player = await client.fetch_user(int(chosen_player_id))
                 
+                time.sleep(2)
                 await message.channel.send(f"{chosen_player.name} has been selected to play medic.")
                 self.db.set_immune(chosen_player_id)
             else:
@@ -37,13 +40,14 @@ class MedicPicker(BaseCommand):
     def find_team(self, message):
         author = message.author
         teams = [x for x in settings.CURRENT_GAME.values()]
-        red, blue = teams[0], teams[1]
-        print(red, blue)
+        if len(teams) > 0:
+            red, blue = teams[0], teams[1]
+            print(red, blue)
 
-        if str(author.id) in red:
-            return red
-        elif str(author.id) in blue:
-            return blue
+            if str(author.id) in red:
+                return red
+            elif str(author.id) in blue:
+                return blue
         return None
 
     def parse_mention(self, mention: str):
