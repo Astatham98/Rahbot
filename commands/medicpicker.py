9941475bd5e83a21d65de3dbe740fe488d95f7f9
Astatham98 <alex.statham1998@gmail.com>
2022-01-06 17:08:09 +0000

    Feature to allow for manual immunity

--- commands/medicpicker.py
+++ commands/medicpicker.py
@@ -12,29 +12,42 @@ class MedicPicker(BaseCommand):
         super().__init__(description, params)
 
     async def handle(self, params, message, client):
-        team = self.find_team(message)
-        if team is not None:
-            eligible = []
-            for player in team:
-                print(player, type(player))
-                if self.db.immune(player) is False:
-                    eligible.append(player)
-            
-            chosen_player_id = random.choice(eligible)
-            chosen_player = await client.fetch_user(int(chosen_player_id))
-            
-            await message.channel.send(f"{chosen_player.name} has been selected to play medic.")
-            self.db.set_immune(chosen_player_id)
+        if len(params) > 0:
+            id = self.parse_mention(params[0])
+            self.db.set_immune(id)
+
+            chosen_player = await client.fetch_user(int(id))
+            await message.channel.send(f'{chosen_player.name} has been given immunity.')
         else:
-            await message.channel.send('Unable to find the corresponding team.')
+            team = self.find_team(message)
+            if team is not None:
+                eligible = []
+                for player in team:
+                    if self.db.immune(player) is False:
+                        eligible.append(player)
+                
+                chosen_player_id = random.choice(eligible)
+                chosen_player = await client.fetch_user(int(chosen_player_id))
+                
+                await message.channel.send(f"{chosen_player.name} has been selected to play medic.")
+                self.db.set_immune(chosen_player_id)
+            else:
+                await message.channel.send('Unable to find the corresponding team.')
        
     def find_team(self, message):
         author = message.author
         teams = [x for x in settings.CURRENT_GAME.values()]
         red, blue = teams[0], teams[1]
+        print(red, blue)
 
         if str(author.id) in red:
             return red
         elif str(author.id) in blue:
             return blue
         return None
+
+    def parse_mention(self, mention: str):
+        """turns a mention into an id string"""
+        id = mention.replace('>', '')
+        id = id.replace('<@', '')
+        return id
