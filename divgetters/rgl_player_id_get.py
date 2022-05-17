13752dd5463911e704a772d0db76dc9832885209
Astatham98 <alex.statham1998@gmail.com>
2022-05-17 10:27:45 +0100

    Updated headers for rgl script

--- divgetters/rgl_player_id_get.py
+++ divgetters/rgl_player_id_get.py
@@ -17,9 +17,18 @@ class RGL():
         "Amateur": 5, 
         "Newcomer": 6 
         }
+        
+        headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Dnt": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        }
 
         req = requests.Session()
-        doc = req.get(link).text
+        doc = req.get(link, headers=headers).text
         soup = BeautifulSoup(doc, 'lxml')
 
         div_table = []
